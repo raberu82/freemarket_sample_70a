27b2c1c943a3081935061d1e6b7d86f661a74b88
raberu82 <happiest8282@gmail.com>
2020-03-05 16:25:47 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
 # ../が一つ増えている
-app_path = File.expand_path('../../', __FILE__)
+app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 # currentを指定
-working_directory "#{app_path}/current"
+working_directory "#{app_path}/config/current"
 
 # それぞれ、sharedの中を参照するよう変更
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
