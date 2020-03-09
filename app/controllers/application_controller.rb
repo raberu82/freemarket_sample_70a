649538415f740031243f48eb1297e403912e80b7
shuto24 <n70.oobashuto@gmail.com>
2020-03-09 20:23:06 +0900

    user-view2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age])
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
 
