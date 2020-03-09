238de9172668155371e0c9f738ece7ce1dfb468f
raberu82 <happiest8282@gmail.com>
2020-03-09 20:27:33 +0900

    add mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
   root 'homes#index'
+  
+  resources :users, only: [:index, :show, :destroy]
+  resources :logouts, only: [:index]
+  resources :cards, only: [:index]
+
 
 end
