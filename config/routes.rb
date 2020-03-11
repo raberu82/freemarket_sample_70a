be50dc33d47ee4b48c1a8e85563281da03478d09
shuto24 <n70.oobashuto@gmail.com>
2020-03-11 21:06:30 +0900

    user-manage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  root to: "posts#index"
-
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
@@ -8,4 +7,5 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+  root to: "posts#index"
 end
