0bfbcffef51b1258d9d48fc214312f70cca412b3
raberu82 <happiest8282@gmail.com>
2020-03-11 14:52:40 +0900

    add credit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'card/new'
+  get 'card/show'
   root 'posts#index'
   resources :users, only: [:index, :show, :destroy]
   resources :logouts, only: [:index]
-  resources :cards, only: [:index]
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
