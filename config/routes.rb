088630a8813f9b5e1f502222042a50e397633d80
mossam628 <mosososo628@gmail.com>
2020-03-11 21:34:26 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new]
-  resources :users, only: [:index, :show, :destroy]
-  resources :logouts, only: [:index]
-  resources :card, only: [:index]
-  resources :cards, only: [:index]
-
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   devise_scope :user do
-
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
+  resources :posts, only: [:index, :show, :new]
+  resources :users
+  resources :logouts, only: [:index]
+  resources :card, only: [:index]
+  resources :cards, only: [:index]
+
+  
   root to: "posts#index"
 
 end
