aa1a1cfee72ec4683126a098851cdee6663e6436
mossam628 <mosososo628@gmail.com>
2020-03-12 12:58:40 +0900

    item

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
   
   def show
+    @item = Item.find(params[:id])
   end
 end
