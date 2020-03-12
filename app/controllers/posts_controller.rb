e7b261d9925c5b2e91543d95515d11e162e3838e
mossam628 <mosososo628@gmail.com>
2020-03-12 18:55:38 +0900

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,11 +5,25 @@ class PostsController < ApplicationController
 
   def new
     @item = Item.new
+    @item_image = @item.item_images.build
+  end
+
+  def create
+    @item = Item.new(product_params)
+    if @item.save
+      binding.pry
+      params[:item_images]['name'].each do |a|
+        @item_image = @item.item_images.create!(name: a)
+      end
+      redirect_to root_path, notice: '出品しました。'
+    else
+      render :new
+    end
   end
   
   def show
     @item = Item.find(params[:id])
     @user = User.find(params[:id])
-    # @images = Item_image.find(params[:id])
+    @item_image = @item.item_images.build
   end
 end
