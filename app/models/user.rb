92bafeb5b03fbf28f7a0a4ca09b4bd111d0621eb
shuto24 <n70.oobashuto@gmail.com>
2020-03-12 16:41:41 +0900

    comit-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, :password , :email , presence: true
-  has_one :address
+  has_one :profile
+  accepts_nested_attributes_for :profile
 end
