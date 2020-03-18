22896cc844f851ba34b01d4ed8effbe16b88b737
mossam628 <mosososo628@gmail.com>
2020-03-18 16:25:58 +0900

    destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,7 +48,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
     if @item.destroy
       redirect_to root_path, notice: '商品情報を削除しました'
     else
