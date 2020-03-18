7e5acc84a63d6a9f4ab2a366194859abf015fa0d
shuto24 <n70.oobashuto@gmail.com>
2020-03-18 16:20:30 +0900

    Revert "user-manage"
    This reverts commit 97b6586e5959ce9253fc3f0899b49c3ac3e647ca.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,16 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-
-  # 全角ひらがなカタカナ漢字
-  VALID_NAME_REGEX = /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/
-  # 全角カタカナ
-  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/
-
-  validates :last_name,       presence: true, format: { with: VALID_NAME_REGEX }
-  validates :first_name,      presence: true, format: { with: VALID_NAME_REGEX }
-  validates :last_name_kana,  presence: true, format: { with: VALID_KANA_REGEX }
-  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
-  validates :postcode,        presence: true
-  validates :prefecture_id,   presence: true
-  validates :city,            presence: true
+  validates :first_name, :last_name , :first_name_kana , :last_name_kana , :postcode , :prefecture_id , :city , :block , presence: true
 end
