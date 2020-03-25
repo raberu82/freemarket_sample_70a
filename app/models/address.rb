8c3d517141e179c0462a89a0ede0334df1eec591
shuto24 <n70.oobashuto@gmail.com>
2020-03-25 18:57:23 +0900

    バリデーション修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -10,8 +10,8 @@ class Address < ApplicationRecord
   validates :first_name,      presence: true, format: { with: VALID_NAME_REGEX }
   validates :last_name_kana,  presence: true, format: { with: VALID_KANA_REGEX }
   validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
-  validates :postcode, presence: true
-  validates :prefecture_id, presence: true
-  validates :city, presence: true
+  validates :postcode,        presence: true
+  validates :prefecture_id,   presence: true
+  validates :city,            presence: true, format: { with: VALID_NAME_REGEX }
   validates :block, presence: true
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -10,7 +10,7 @@ class Profile < ApplicationRecord
   validates :first_name,      presence: true, format: { with: VALID_NAME_REGEX }
   validates :last_name_kana,  presence: true, format: { with: VALID_KANA_REGEX }
   validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX }
-  validates :birth_year,     presence: true
-  validates :birth_month,    presence: true
-  validates :birth_day,      presence: true
+  validates :birth_year,      presence: true
+  validates :birth_month,     presence: true
+  validates :birth_day,       presence: true
 end
